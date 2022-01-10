b19e4d1d0ae617d245d155161a5525b9b45f5b9c
Andres David Mendoza Marquez <andres.david.mm@hotmail.com>
2022-01-09 19:27:26 -0500

    first commit

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    is_available = models.BooleanField()
+
+    def __str__(self):
+        return self.name
---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Product
+from rest_framework import serializers, viewsets
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'category', 'price', 'is_available')
+        
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
