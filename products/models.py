091d4bb5cc2e0897339c378cbaa140b3c2231ec0
Andres Mendoza <andres.david.mm@hotmail.com>
2022-10-14 15:00:41 -0500

    added filter by ip

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     category = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     isAvailable = models.BooleanField()
+    created_by = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from config.get_client_ip import get_client_ip
 from .models import Product
 from rest_framework import serializers, viewsets
 
@@ -9,6 +12,27 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ('id', 'name', 'category', 'price', 'isAvailable')
         
+class CreateProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'category', 'price', 'isAvailable', 'created_by')
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(created_by=get_client_ip(request))
+        serialized = ProductSerializer(queryset, many=True)
+        return Response(serialized.data)
+
+    def create(self, request, *args, **kwargs):
+        request.data['created_by']=get_client_ip(request)
+        serialized = CreateProductSerializer(data=request.data)
+        if not serialized.is_valid():
+            return Response(status = status.HTTP_400_BAD_REQUEST, data = serialized.errors)
+        else:
+            serialized.save()
+            serialized = ProductSerializer(serialized.data)
+            return Response(status = status.HTTP_201_CREATED, data = serialized.data)
+            
