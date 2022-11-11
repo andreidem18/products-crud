bc4c7d655cc2330d5a3ff9b8cf509da21df2e49a
Andres Mendoza <andres.david.mm@hotmail.com>
2022-11-10 19:40:26 -0500

    added request limit

--- config/settings.py
+++ config/settings.py
@@ -99,6 +99,17 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '30/minute',
+        'user': '30/minute'
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
